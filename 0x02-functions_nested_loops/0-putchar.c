fe4ed360c0cf1ceda76c4644d2cf42bbc098557a
Sonwabile <sonwabilecwala6@gail.com>
2022-12-14 06:26:42 -0800

    program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * mtain -Entry point
+ *
+ * Description: prints _putchar
+ *
+ * Always retun 0 
+ */
+
+int main(void)
+{
+	
+	int letter;
+	printf("press Enter");
+	getchar();
+	letter = '_';
+	putchar(letter);
+	letter = 'p';
+	putchar(letter);
+	letter = 'u';
+	putchar(letter);
+	letter = 't';
+	putchar(letter);
+	letter = 'c';
+	putchar(letter);
+	letter = 'h';
+	putchar(letter);
+	letter = 'a';
+	putchar(letter);
+	letter = 'r';
+	printf("\n");
+	return (0);
+}
