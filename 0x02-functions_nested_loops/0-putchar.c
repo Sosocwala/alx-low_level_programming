a4d4040820dac52c4500c4e0f1e3f688997d8f2f
Sonwabile <sonwabilecwala6@gail.com>
2022-12-15 23:28:12 -0800

    program that prints

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,33 +1,22 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * mtain -Entry point
- *
- * Description: prints _putchar
- *
- * Always retun 0 
- */
+* main - print _putchar
+* is written by mbah
+* Return: 0
+*/
 
 int main(void)
 {
-	
-	int letter;
-	printf("press Enter");
-	getchar();
-	letter = '_';
-	putchar(letter);
-	letter = 'p';
-	putchar(letter);
-	letter = 'u';
-	putchar(letter);
-	letter = 't';
-	putchar(letter);
-	letter = 'c';
-	putchar(letter);
-	letter = 'h';
-	putchar(letter);
-	letter = 'a';
-	putchar(letter);
-	letter = 'r';
-	printf("\n");
-	return (0);
+
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return (0);
 }
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-read me
+_putchar
