d993b3fa3c9bcdf34114a42eef5ba16c415dbfcd
Sonwabile <sonwabilecwala6@gail.com>
2022-12-16 15:59:52 -0800

    function that computes the absolute value of an integer

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * print_sign - print the sign of a number
- * @n: the numberto to be checked for
- * Returns: 1 and prints + if n is greater than zero
+ * @n: the number to be checked for
+ * Return: 1 and prints + if n is greater than zero
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
  */
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*_abs - computes the abs value of an int
+*@n: is the int in question
+*
+*Return: the abs of the int.
+*/
+int _abs(int)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
